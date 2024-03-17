a015825cd2a771661f66fce45703855ed51a54e0
ChrisJohnsonCD <129870653+ChrisJohnsonCD@users.noreply.github.com>
2024-03-17 10:07:24 -0600

    MainPage_LayoutDone

--- scr/App.jsx
+++ scr/App.jsx
@@ -1,39 +1,17 @@
-/**
- * My To Do List App
- *
- * @format
- */
+
+
 
 import React from 'react';
-import {
-  
-  StyleSheet
-} from 'react-native';
-
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import MainScreen from './screens/MainPage';
-import InstructionsScreen from './screens/InstructionsPage';
-import GamePage from './screens/GamePage';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import MainPage from './screens/MainPage';
 
 function App() {
-  // const Stack = createNativeStackNavigator();
-
-  
-
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-            <Stack.Screen name="Main" component={MainScreen} />
-            <Stack.Screen name="Instructions" component={InstructionsScreen} />
-            <Stack.Screen name="GamePage" component={GamePage} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <MainPage />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
-
-
 export default App;
+
