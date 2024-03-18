94d0161ae675d8939c7d8aba46edf3edef3a1197
Ruvalpreet <123599263+Ruvalpreet@users.noreply.github.com>
2024-03-17 18:11:17 -0600

    GamePage

---
+++ scr/screens/DisplayKeys.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {Pressable, StyleSheet, Text} from 'react-native';
+import {useEffect} from 'react';
+const DisplayKeys = ({guessedWord, value}) => {
+  const [text, setText] = React.useState([]);
+
+  useEffect(() => {
+    setText([
+      'q',
+      'w',
+      'e',
+      'r',
+      't',
+      'y',
+      'u',
+      'i',
+      'o',
+      'p',
+      'a',
+      's',
+      'd',
+      'f',
+      'g',
+      'h',
+      'j',
+      'k',
+      'l',
+      'z',
+      'x',
+      'c',
+      'v',
+      'b',
+      'n',
+      'm',
+    ]);
+  }, [value]);
+
+  const handleDeleteItem = index => {
+    const newText = [...text];
+    newText.splice(index, 1);
+    setText(newText);
+  };
+
+  return text.map((letter, index) => {
+    return (
+      <Pressable
+        key={index}
+        style={styles.key}
+        onPress={() => {
+          guessedWord(letter);
+          handleDeleteItem(index);
+        }}>
+        <Text style={styles.keyText}>{letter}</Text>
+      </Pressable>
+    );
+  });
+};
+
+const styles = StyleSheet.create({
+  key: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
+  },
+  keyText: {
+    fontSize: 24,
+    color: '#000',
+  },
+});
+
+export default DisplayKeys;
