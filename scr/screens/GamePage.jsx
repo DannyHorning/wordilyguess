995938f4558d4f04c7953c1fa1c439f648cde45c
Danny Horning <danny-horning@hotmail.com>
2024-03-30 16:03:01 -0600

    rendering of cowboy and styling changes

--- scr/screens/GamePage.jsx
+++ scr/screens/GamePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Pressable, StyleSheet, Text, View, TextInput, Image} from 'react-native';
 import {useState} from 'react';
 import {useEffect} from 'react';
 import hangmanWords from '../layouts/assets/hangmanWords.json';
@@ -7,10 +7,11 @@ import MainLayout from '../layouts/Layout';
 import DisplayKeys from './DisplayKeys';
  
 const GamePage = () => {
+  const maxLives = 6;
   const [value, setValue] = useState(''); // store the string of the word
   const [chars, setChars] = useState([]); // store the characters of the word
   const [guessedChar, setGuessedChar] = useState([]); // store the guessed characters of the word
-  const [life, setLife] = useState(6); // store the life count
+  const [life, setLife] = useState(maxLives); // store the life count
 
 
   
@@ -26,6 +27,15 @@ const GamePage = () => {
   useEffect(() => {
     setGuessedChar(Array.from({length: chars.length}, () => ' '));
   }, [chars]); 
+
+  const hangmanImages = [
+    require('../layouts/assets/cowboy-head.png'),
+    require('../layouts/assets/cowboy-body.png'),
+    require('../layouts/assets/cowboy-arm1.png'),
+    require('../layouts/assets/cowboy-arm2.png'),
+    require('../layouts/assets/cowboy-leg2.png'),
+    require('../layouts/assets/cowboy-leg.png'),
+  ];
 
   const handleGameEnd = () => {
     if (life <= 0){
@@ -110,41 +120,49 @@ const GamePage = () => {
   };
   
   return (
-    // for testing purpose adds life count and answer at the top
     <MainLayout>
-      
-      <View>
-        <Text style={{fontSize: 20}}>{value}</Text>
-        <Text style={{fontSize: 20}}>{life}</Text>
-        <Text style={{fontSize: 20}}>{guessedChar.join('')}</Text>
-      </View>
-
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>{handleGameEnd()}</View>
-
-      <View style={[styles.outer, { marginTop: '60%' }]}>
-        <View style={styles.inner}>
-          {guessedChar.map((guessChar, index) => (
-            <Text key={index} style={styles.innerText}>
-              {guessChar}
-            </Text>
-          ))}
+      <View style={{ position: 'relative', flex: 1 }}>
+        <View>
+          <Text style={{ fontSize: 20 }}>{value}</Text>
+          <Text style={{ fontSize: 20 }}>{life}</Text>
+          <Text style={{ fontSize: 20 }}>{guessedChar.join('')}</Text>
         </View>
-      </View>
- 
-      <View>
-        <TextInput />
-      </View>
-      {life > 0 && guessedChar.join('') !== value && (
-      <View style={styles.outer}>
-        <View style={styles.inner}>
-          <DisplayKeys guessedWord={guessedWord} value={value} />
+  
+        <View style={{ justifyContent: 'center', alignItems: 'center', position: 'absolute', top: '20%', left: '38%', transform: [{ translateX: -50 }, { translateY: -50 }], zIndex: 1 }}>
+          {handleGameEnd()}
         </View>
-      </View>)}
-
-      
+  
+        {Array.from({ length: maxLives - life }, (_, index) => (
+          <View key={index} style={{ justifyContent: 'center', alignItems: 'center', position: 'absolute', top: '-5%', left: '25%', transform: [{ translateX: -50 }, { translateY: -50 }] }}>
+            <Image source={hangmanImages[index]} style={{ width: 600, height: 600 }} />
+          </View>
+        ))}
+  
+        <View style={[styles.outer, { marginTop: '60%' }]}>
+          <View style={styles.inner}>
+            {guessedChar.map((guessChar, index) => (
+              <Text key={index} style={styles.innerText}>
+                {guessChar}
+              </Text>
+            ))}
+          </View>
+        </View>
+  
+        <View>
+          <TextInput />
+        </View>
+  
+        {life > 0 && guessedChar.join('') !== value && (
+          <View style={styles.outer}>
+            <View style={styles.inner}>
+              <DisplayKeys guessedWord={guessedWord} value={value} />
+            </View>
+          </View>
+        )}
+      </View>
     </MainLayout>
   );
-};
+};    
  
 const getRandomWord = ({category}) => {
   const words = hangmanWords.filter(word => word.category === category);
